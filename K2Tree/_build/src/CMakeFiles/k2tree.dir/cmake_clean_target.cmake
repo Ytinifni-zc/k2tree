26419cbe67f9e2a124bdbeb9525f91c6d6533697
Ytinifni-zc <zhcheng127@gmail.com>
2017-12-25 16:31:59 +0800

    clean.

--- K2Tree/_build/src/CMakeFiles/k2tree.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libk2tree.a"
-)
--- K2Tree/_build/tests/CMakeFiles/tests_utils.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "tests_utils.pdb"
-  "tests_utils"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tests_utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
